31b0a4023932e90d0ee3237338594d37fd316b15
kathyleboeuf <kleboeuf@townisp.com>
2018-07-23 17:38:11 -0400

    part 3 done

--- shopping-list-week-2-master/scripts/store.js
+++ shopping-list-week-2-master/scripts/store.js
@@ -1,4 +1,5 @@
 'use strict';
+/* global item, Item,  cuid*/
 
 const store = (function () {
   const foo = 'bar';
@@ -10,13 +11,47 @@ const store = (function () {
   ];
   const hideCheckedItems = false;
   const searchTerm = '';
+  //??????????????????????????????????????
+  const findById = function(id) {
+    return store.items.find(id);
+  };
+  const addItem = function(name) {
+    console.log(Item);
+    try {
+      Item.validateName(name);
+      this.items.push(Item.create(name));
+    } catch(error) {
+      console.log('Cannot add item: ' + error.message);
+    }
+  };
+  const findAndToggleChecked = function(id) {
+    const findId = this.findById(id);
+    findId.checked = !findId.checked;
+  };
+
+  const findAndUpdateName = function(id, newName) {
+    try {
+      Item.validateName(newName);
+      const itemId = findById(id);
+      itemId.name = newName;
+    } catch(error) {
+      console.log('Cannot update name : ' + error.message);
+    }
+  };
+  const findAndDelete = function(id) {
+
+    const index = this.items.findIndex(item => item.id === id);
+    this.items.splice(index, 1);
+  };
 
   return {
     items: items,
     hideCheckedItems: hideCheckedItems,
-    searchTerm: searchTerm
+    searchTerm: searchTerm,
+    findById: findById,
+    addItem : addItem,
+    findAndToggleChecked: findAndToggleChecked,
+    findAndUpdateName: findAndUpdateName,
+    findAndDelete  : findAndDelete
   };
-
-
-
 }() );
